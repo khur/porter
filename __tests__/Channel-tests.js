04c571f2e4821997d8c532bdd530ca0468d80890
Kyle Hurst <kylehurst42@gmail.com>
2016-02-29 10:43:54 -0700

    WIP: testing suite and documentation

--- __tests__/Channel-tests.js
+++ __tests__/Channel-tests.js
@@ -5,20 +5,13 @@ const Channel     = require("../src/Channel").default;
 describe('Channel', function(){
 
     let testChannel = new Channel('testchannel');
-    // beforeEach(function(){
-    //   testChannel.add()
-    // })
-
-    // afterEach(function(){
-    //   testChannel.clear()
-    // })
-
-    xit('should have publish function', function(){
+    
+    it('should have publish function', function(){
       console.warn('pending publish test');
 
     }); // gets message
 
-    xit('should have grab function', function(){
+    it('should have grab function', function(){
       console.warn('pending grab function test');
     });
 
@@ -27,34 +20,52 @@ describe('Channel', function(){
     
     it('should be able to be subscribed to', function(){
 
-      testChannel.subscribe("testaction", function(data){
+      testChannel.subscribe("testSubscribe", function(data){
         let details = data.detail;
+        expect(details).toBeDefined();
         expect(details.blah).toEqual(true);
       });
 
-      testChannel.publish("testaction", {blah: true});
+      expect(Object.keys(testChannel.subscribers).length).toEqual(1);
+
+      testChannel.publish("testSubscribe", {blah: true});
 
     }); // check subscribers here
 
-    xit('should clear subscribers', function(){
+    it('should clear subscribers', function(){
       console.warn('pending clear subscribers test');
-    
-
     });
-    xit('should be able to be unsubscribed to', function(){
-      console.warn('pending unsubscribing test')
+
+    it('should be able to be unsubscribed to', function(){
+
+      testChannel.subscribe("testUnsubscribe", function(data){
+        let details = data.detail;
+        expect(details).toBeDefined();
+        expect(details.subscribedTo).toEqual(true);
+      });
+
+      testChannel.unsubscribe("testUnsubscribe", function(data){
+        console.warn("unsubscribe", data);
+      });
+
+      // expect(Object.keys(testChannel.subscribers).length).toEqual(1);
+      console.warn('num of subscribers', Object.keys(testChannel.subscribers).length);
+
+      testChannel.publish("testUnsubscribe", {subscribedTo: true});
+
+
+
     }); // check subscribers here
 
   });
- 
+
 
   describe('Formatting action', function(){
 
-    xit('should format action with ::', function(){
+    it('should format action with ::', function(){
       expect(testChannel.formatAction('formatted')).toEqual('testchannel::formatted');
     });
 
-    
   });
 
 
